7ac302e53556455576f3d5a378f63e4750b055e4
emamagic <developermagic7@gmail.com>
2021-07-31 21:28:48 +0430

    first implementation

---
+++ message_input/src/main/java/com/emamagic/messageinput/ReplyBar.java
@@ -0,0 +1,64 @@
+package com.emamagic.messageinput;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import com.emamagic.chatbox.ViewHelper;
+
+public class ReplyBar extends RelativeLayout {
+
+    private ImageView closeBtn;
+    private FontTextView title;
+    private FontTextView summery;
+
+    public ReplyBar(Context context) {
+        super(context);
+        init();
+    }
+
+    public ReplyBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ReplyBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        if (isInEditMode()) return;
+        inflate(getContext(), R.layout.reply_header_layout, this);
+        ViewGroup.LayoutParams params = new LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewHelper.dpToPx(48)
+        );
+        setLayoutParams(params);
+
+        closeBtn = findViewById(R.id.close);
+        title = findViewById(R.id.title);
+        summery = findViewById(R.id.summery);
+    }
+
+    public void setText(String text) {
+        summery.setText(text);
+    }
+
+    public void enterEditMode() {
+        title.setText(R.string.edit_message);
+    }
+
+    public void enterReplyMode(String userName) {
+        title.setText(userName);
+    }
+
+    public void setOnCloseListener(OnCloseListener onCloseListener) {
+        closeBtn.setOnClickListener(e -> onCloseListener.onReplyBarDismiss());
+    }
+
+    public interface OnCloseListener {
+        void onReplyBarDismiss();
+    }
+}
---
+++ message_input/src/main/java/com/emamagic/messageinput/Style.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright 2016 limoo.com
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package com.emamagic.messageinput;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+import androidx.annotation.ColorRes;
+import androidx.annotation.DimenRes;
+import androidx.annotation.DrawableRes;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Base class for chat component styles
+ */
+public abstract class Style {
+
+    protected Context context;
+    protected Resources resources;
+    protected AttributeSet attrs;
+
+    protected Style(Context context, AttributeSet attrs) {
+        this.context = context;
+        this.resources = context.getResources();
+        this.attrs = attrs;
+    }
+
+    protected final int getDimension(@DimenRes int dimen) {
+        return resources.getDimensionPixelSize(dimen);
+    }
+
+    protected final int getColor(@ColorRes int color) {
+        return ContextCompat.getColor(context, color);
+    }
+
+    protected final Drawable getDrawable(@DrawableRes int drawable) {
+        return ContextCompat.getDrawable(context, drawable);
+    }
+
+    protected final Drawable getVectorDrawable(@DrawableRes int drawable) {
+        return ContextCompat.getDrawable(context, drawable);
+    }
+
+}
